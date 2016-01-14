0d648ecdd8e9af9a23e6b7b0270aadb200a5a294
Dom <fizlaz@gmail.com>
2016-01-14 20:47:05 +0100

    problem 1 0.9

--- PS1/genData.R
+++ PS1/genData.R
@@ -1,16 +1,40 @@
-f <- function(obs=100,savecsv=TRUE,savepdf=TRUE){
+xfactor <- function(obsx=1000,obsy=1000,obsz=1000,mux=c(2,2,2),rhox=0.9,sdx1=2,savecsv=TRUE,savepdf=TRUE,seed=1234){
+  
   library(mvtnorm)
-  library(ggplot2)
+  library(scatterplot3d)
   
-  if(savecsv=TRUE){
-    write.csv(dataaa, file = "dataset.csv", row.names=FALSE)
+  set.seed(seed)
+
+  covTerm <- rhox * sdx1^2
+  cvc <- matrix(c(sdx1^2,covTerm,covTerm,covTerm,sdx1^2,covTerm,covTerm,covTerm,sdx1^2),ncol = 3)
+  x3d <- rmvnorm(obsx,mean = mux,sigma = cvc)
+
+  cvc <- matrix(c(sdx1^2,-covTerm,-covTerm,-covTerm,sdx1^2,covTerm,-covTerm,covTerm,sdx1^2),ncol = 3)
+  y3d <- rmvnorm(obsy,mean = mux,sigma = cvc)
+
+  cvc <- matrix(c(sdx1^2,covTerm,-covTerm,covTerm,sdx1^2,-covTerm,-covTerm,-covTerm,sdx1^2),ncol = 3)
+  z3d <- rmvnorm(obsz,mean = mux,sigma = cvc)
+  
+  lab3d <- c(rep(1,obsx),rep(2,obsy),rep(3,obsz))
+  lab3da <- c(rep("a",obsx),rep("b",obsy),rep("c",obsz))
+  
+  dat3d <- data.frame(rbind(x3d,y3d,z3d))
+  dat3d <- data.frame(dat3d,lab3d)
+  dat3d <- data.frame(dat3d,lab3da)
+  
+  
+  if(savecsv==TRUE){
+    write.csv(dat3d, file = "dataset.csv", row.names=FALSE)
   }
   
-  if(savepdf=TRUE){
+  if(savepdf==TRUE){
     pdf("dataPlot.pdf")
-    plot()
-    dev.off() #return output to terminal
+    s3d <- with(dat3d, scatterplot3d(X1, X2, X3, color = as.numeric(lab3d),
+                                     col.axis = "blue",col.grid = "lightblue"))
+    legend(s3d$xyz.convert(11, 0.7, 0.5), pch = 19, yjust=0,
+           legend = levels(dat3d$lab3da), col = seq_along(levels(dat3d$lab3da)))
+    dev.off() 
   }
   
-  return(list())
+  return(dat3d)
 }
---
+++ PS1/loanData3C.R
@@ -0,0 +1,33 @@
+library(mvtnorm)
+library(ggplot2)
+library(extrafont)
+
+sigmaXY <- function(rho, sdX, sdY) {
+  covTerm <- rho * sdX * sdY
+  VCmatrix <- matrix(c(sdX^2, covTerm, covTerm, sdY^2), 
+                     2, 2, byrow = TRUE)
+  return(VCmatrix)
+}
+
+genBVN <- function(n = 1, seed = NA, muXY=c(0,1), sigmaXY=diag(2)) {
+  if(!is.na(seed)) set.seed(seed)
+  rdraws <- rmvnorm(n, mean = muXY, sigma = sigmaXY)
+  return(rdraws)
+}
+
+loanData <- function(noApproved, noDenied, muApproved, muDenied, sdApproved, 
+                     sdDenied, rhoApproved, rhoDenied, seed=1111) {
+  sigmaApproved <- sigmaXY(rho=rhoApproved, sdX=sdApproved[1], sdY=sdApproved[2])
+  sigmaDenied <- sigmaXY(rho=rhoDenied, sdX=sdDenied[1], sdY=sdDenied[2])
+  approved <- genBVN(noApproved, muApproved, sigmaApproved, seed = seed)
+  denied <- genBVN(noDenied, muDenied, sigmaDenied, seed = seed+1)
+  loanDf <- as.data.frame(rbind(approved,denied))
+  deny <- c(rep("Approved", noApproved), rep("Denied", noDenied))
+  target = c(rep(0, noApproved), rep(1, noDenied))
+  loanDf <- data.frame(loanDf, deny, target)
+  colnames(loanDf) <- c("PIratio", "solvency", "deny", "target")
+  return(loanDf)
+}
+
+loanDf <- loanData(noApproved=50, noDenied=50, c(4, 150), c(10, 100), 
+                   c(1,20), c(2,30), -0.1, 0.6, 1221)
